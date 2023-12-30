0a474ee985a7e0f7c2733f08724fe2f45d7e07b6
IRADUKUNDA Allelua Fiacre <firaduk@yahoo.com>
2023-12-30 21:52:19 +0200

    --no-edit

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
             </div>
           ))}
         </div>
-        <div>
+        <div className="py-10">
           <Highlights />
         </div>
         <div>
