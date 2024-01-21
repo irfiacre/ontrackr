190763904d10395b2b05003e3faa5e219c058d62
IRADUKUNDA Allelua Fiacre <firaduk@yahoo.com>
2024-01-21 22:18:01 +0200

    improvements

--- app/components/Cards/DayThemeCard.tsx
+++ app/components/Cards/DayThemeCard.tsx
@@ -11,6 +11,13 @@ const DayThemeCard = ({ ...props }: CardProps) => {
   return (
     <div className={`card bg-primary-dark-2 text-primary-content p-3`}>
       <div className="card-body text-white">
+        <input
+          type="text"
+          name="xxxx"
+          id="xxx"
+          className="bg-primary-dark-2 "
+          value={props.dayName}
+        />
         <h2 className="card-title">{props.dayName}</h2>
         <p>{props.themeName}</p>
       </div>
