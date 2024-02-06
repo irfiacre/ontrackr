111bda4254103a34610116976048f31c8cb88442
IRADUKUNDA Allelua Fiacre <firaduk@yahoo.com>
2024-02-06 22:16:29 +0200

    improve profile

--- app/components/LayoutCompontents/Sidebar.tsx
+++ app/components/LayoutCompontents/Sidebar.tsx
@@ -102,6 +102,27 @@ const Sidebar = () => {
               <div className="w-10 rounded-full">
                 <img alt="User Img" src={userInfo.image} />
               </div>
+              {/* <div
+                className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="menu-button"
+                tabIndex={-1}
+              >
+                <div className="py-1" role="none">
+                  <form method="POST" action="#" role="none">
+                    <button
+                      type="submit"
+                      className="text-gray-700 block w-full px-4 py-2 text-left text-sm"
+                      role="menuitem"
+                      tabIndex={-1}
+                      id="menu-item-3"
+                    >
+                      Sign out
+                    </button>
+                  </form>
+                </div>
+              </div> */}
             </div>
             <strong className="text-xl tracking-wide truncate text-center align-middle text-primary-dark-1">
               {`${userInfo.firstName} ${userInfo.lastName}`}
